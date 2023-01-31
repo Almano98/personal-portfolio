2a2343bbb4fe7b5d5215a9029fd4c56ddec7dca2
Matthew Almano <matthew.almano51@gmail.com>
2023-01-31 11:22:42 +0200

    [ADDED] React animate on scroll library. [REMOVED] Unused code in Skill Section

--- src/components/SkillsSection/index.js
+++ src/components/SkillsSection/index.js
@@ -3,19 +3,13 @@ import {
   InformationContainer,
   InformationContent,
   SkillsContainer,
-  SkillTags,
 } from "./SkillElements";
 import { SectionHeader, SectionHeaderText } from "../common/SectionElements";
 import { FilterButton, ButtonContainer } from "../common/ButtonElements";
 import skills from "../../data/skills-data.json";
 
-// TODO Create dynamic skills section based on selected filter.
 const SkillSection = () => {
   const [activeSkill, setActiveSkill] = useState("0");
-  const [activeDescription, setActiveDescription] = useState(
-    skills[0].description
-  );
-
   const handleClick = (event) => {
     setActiveSkill(event.target.id);
   };
@@ -30,7 +24,7 @@ const SkillSection = () => {
             <FilterButton
               key={skill.id}
               id={skill.id}
-              active={activeSkill == skill.id}
+              active={activeSkill === skill.id}
               onClick={handleClick}
             >
               {skill.name}
