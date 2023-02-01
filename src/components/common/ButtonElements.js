c030b8c7a2574ff4e6ca26ff3d1565a16406a2d3
Matthew Almano <matthew.almano51@gmail.com>
2023-02-01 15:14:06 +0200

    [UPDATED] Filter button styling on smaller devices

--- src/components/common/ButtonElements.js
+++ src/components/common/ButtonElements.js
@@ -22,7 +22,7 @@ export const FilterButton = styled.button`
 
   @media screen and (max-width: 768px) {
     padding: 0.2rem 1rem;
-    margin: 0 0.5rem;
+    margin: 0.5rem;
     width: 10rem;
   }
 `;
